3e6c88e54270c3c5ca20ba70c056a3c0d0991c57
leprestige <tsafackeric@gmail.com>
2017-07-14 10:03:11 +0200

    Delete AllTestsStrutturali.java

--- test_strutturali/AllTestsStrutturali.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ testAdministratore.class, testPagamentoEseguito.class, testPostazione.class, testUtente.class })
-public class AllTestsStrutturali {
-
-}
